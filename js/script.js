e115f5d80491059b99945999aef85d1dbed91d80
marischi <marileneifsp@gmail.com>
2023-03-14 09:23:10 -0300

    Projeto mudar fundo finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 let button = document.getElementById('button');
+let fundo_azul = false;
 
 function mudar_fundo(){
-    document.body.style.background = '#40E0D0';
+    if (fundo_azul == false){
+        fundo_azul = true;
+        document.body.style.background = '#40E0D0';
+        button.style.background = 'black';
+        button.style.color = '#40E0D0';
+    }else{
+        fundo_azul= false;
+        document.body.style.background = 'black';
+        button.style.background = '#40E0D0';
+        button.style.color = 'black';
+    }
+    
 }
